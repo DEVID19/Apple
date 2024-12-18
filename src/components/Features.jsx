55c56e8cf9ba9550fc0c0b4e0f26e2f1bd1e125d
DEVID19 <devidbisen84@gmail.com>
2024-12-18 22:03:07 +0530

    Third commit

---
+++ src/components/Features.jsx
@@ -0,0 +1,24 @@
+import { useGSAP } from '@gsap/react'
+import React from 'react'
+
+const Features = () => {
+
+    useGSAP(() => {
+        
+    } , [])
+
+  return (
+      <section className='h-full common-padding  bg-zinc relative overflow-hidden'>
+          
+          <div className='screen-max-width '>
+              <div className='mb-12 w-full '>
+                  <h1 id='features_title' className='section-heading '>
+                      Explore the full Story.
+                  </h1>
+              </div>
+          </div>
+    </section>
+  )
+}
+
+export default Features
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,14 +1,22 @@
-import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  View,
+} from "@react-three/drei";
+
+import * as THREE from "three";
 import Lights from "./Lights";
+import Loader from "./Loader";
+import IPhone from "./IPhone";
 import { Suspense } from "react";
-import IPhone from "./IPhone"
-import * as  THREE from 'three'
+
 const ModelView = ({
   index,
   groupRef,
   gsapType,
   controlRef,
-  setRotationSize,
+  setRotationState,
   size,
   item,
 }) => {
@@ -16,39 +24,38 @@ const ModelView = ({
     <View
       index={index}
       id={gsapType}
-      className={` w-full h-full ${
-        index === 2
-      } ? 'right-[-100%]: ' '  `}
+      className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
     >
-      {/* Ambient light */}
+      {/* Ambient Light */}
       <ambientLight intensity={0.3} />
 
-          <PerspectiveCamera makeDefault position={[0, 0, 4]} />
-          
-          <Lights />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
-          <OrbitControls
-              makeDefault
-              ref={controlRef}
-              enableZoom={false}
-              enablePan={false}
-              rotateSpeed={0.4}
-              target={new THREE.Vector3(0, 0, 0)}
-              onEnd={()=>setRotationState(controlRef.current.getAzimuthalAngle())}
+      <Lights />
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<Loader />}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
           />
-          
-          <group ref={groupRef} name={`${index === 1 } ? 'small'  : 'large' `} position={[0,0,0]}>
-          <Suspense fallback={<div>Loading</div>}>
-                  <IPhone
-                      scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
-                      item={item}
-                      size={size}
-                  />
-          </Suspense>
-          </group>
-
-
-          
+        </Suspense>
+      </group>
     </View>
   );
 };
